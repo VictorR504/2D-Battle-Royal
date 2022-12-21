eb6c3d7ab97ae1daceda3e1914f04874d70b903d
victo <victor.rasmussen.vr@gmail.com>
2022-12-21 09:59:42 +0100

    Change layout file.py

--- CockFight.py
+++ CockFight.py
@@ -1,5 +1,5 @@
 import pygame, sys
-from Player import Player
+from Settings import *
 
 #------------------------------------------------------------------------------#
 #-----------------------------Initialization-----------------------------------#
@@ -7,34 +7,31 @@ from Player import Player
 class Game:
 
     def __init__(self):
-        player_sprite = Player((screen_height/2,screen_width/2),5)
-        self.player = pygame.sprite.GroupSingle(player_sprite)
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH,HEIGTH))
+        self.clock = pygame.time.Clock()
+
+#------------------------------------------------------------------------------#
+#---------------------------------Game Loop------------------------------------#
+#------------------------------------------------------------------------------#
 
     def run(self):
-        self.player.update()
-        self.player.draw(screen)
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            self.screen.fill(('black'))
+            pygame.display.update()
+            self.clock.tick(FPS)
+            pygame.display.set_caption('CockFight')
+            
+
 #------------------------------------------------------------------------------#
-#-------------------------------GAME LOOP--------------------------------------#
+#---------------------------------Runs My Game---------------------------------#
 #------------------------------------------------------------------------------#
+
 if __name__ == '__main__':
-
-    #--------------------------Setup Screen--------------------------#
-    pygame.init()
-    screen_height = 600
-    screen_width = 600
-    screen = pygame.display.set_mode((screen_height,screen_width))
-    pygame.display.set_caption('Cock Fight')
-    #----------------------------------------------------------------#
-    clock = pygame.time.Clock()
     game = Game()
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-
-        screen.fill((30,30,30))
-        game.run()
-        pygame.display.update()
-        clock.tick(60)
+    game.run()
 
